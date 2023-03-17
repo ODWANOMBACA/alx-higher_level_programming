da19b73ae6680c11dea4b41b99d2afe956e46190
odwa <odwanombaca18@gmail.com>
2023-03-17 05:32:08 -0700

    Task 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
--- Creates the MySQL server user user_0d_1
--- which has all privileges on the server
--- with password user_0d_1_pwd
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_od_1@localhost;
+-- Create user
+-- Execute: cat 1-create_user.sql | mysql -hlocalhost -uroot -p
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
