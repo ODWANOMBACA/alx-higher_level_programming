404a7164c692ad399e06d82700a5e45a6086534b
ODWANOMBACA <odwanombaca18@gmail.com>
2023-05-30 11:10:45 -0700

    Task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,36 @@
 #!/usr/bin/python3
+"""
+ a script that lists all states with a name startin
+g with N (upper N) from the database hbtn_0e_0_usa:
+"""
+
 import MySQLdb
 import sys
+"""
+Import the MySQLdb and sys modules
+"""
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
+    """
+    create the connection
+    """
+    db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=db_name)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
+            port=3306,
+            passwd=sys.argv[2],
+            db=sys.argv[3]
+            )
+    cursor_obj = db.cursor()
+    """
+    Creates the cursor object
+    """
+    cursor_obj.execute(
+            "SELECT *  FROM states WHERE name LIKE  BINARY'N%' \
+                    ORDER BY states.id ASC")
+    selected_row = cursor_obj.fetchall()
+    for i in selected_row:
+        print(i)
+    cursor_obj.close()
     db.close()
